1f2f4a98c1c6e1dc9f70d5221f40a038f6c56941
weixin_43383780 <weixin_43383780@noreply.gitcode.com>
2026-04-23 10:44:32 +0800

    update: 更新文件 run_gdn_fwd_o.sh
    Signed-off-by: weixin_43383780 <weixin_43383780@noreply.gitcode.com>

--- torch_custom/aclnn_extension/test/run_gdn_fwd_o.sh
+++ torch_custom/aclnn_extension/test/run_gdn_fwd_o.sh
@@ -19,4 +19,4 @@ dataPath="/path/to/data"
 
 echo 'Case: batch=' $batch ' seqlen=' $seqlen ' kNumHead=' $kNumHead  ' vNumHead=' $vNumHead ' kHeadDim=' $kHeadDim ' vHeadDim=' $vHeadDim ' isVariedLen=' $isVariedLen ' chunkSize=' $chunkSize ' dtype=' $dtype
 python3 test_fwd_o.py $shapeBatch $seqlen $kNumHead $vNumHead $kHeadDim $vHeadDim $isVariedLen $tokenBatch $chunkSize $scale "$dtype" $useActualInput $useActualOutput $dataPath $device $gDtype
-python data_compare_o.py $dtype
+ # python data_compare_o.py $dtype
